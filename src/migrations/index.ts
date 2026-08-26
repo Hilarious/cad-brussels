7fa89a57d18ac11198715cc7df7348ef5df82c56
Hilarious <joker@hilarious.be>
2026-08-26 15:36:29 +0200

    Migration: corrige les appellations protegees dans la base

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260825_215828_ajout_blocs_et_collections from './20260825_215828_ajout_blocs_et_collections';
+import * as migration_20260826_100000_corrige_appellations_en_base from './20260826_100000_corrige_appellations_en_base';
 
 export const migrations = [
   {
     up: migration_20260825_215828_ajout_blocs_et_collections.up,
     down: migration_20260825_215828_ajout_blocs_et_collections.down,
     name: '20260825_215828_ajout_blocs_et_collections'
+  },
+  {
+    up: migration_20260826_100000_corrige_appellations_en_base.up,
+    down: migration_20260826_100000_corrige_appellations_en_base.down,
+    name: '20260826_100000_corrige_appellations_en_base'
   },
 ];
